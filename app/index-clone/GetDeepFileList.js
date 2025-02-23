f05d25e2edd30cb41379859135f8ddcf27822651
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2025-02-23 18:24:45 +0800

    西元2025年02月23日 (週日) 18時24分45秒 CST

--- app/index-clone/GetDeepFileList.js
+++ app/index-clone/GetDeepFileList.js
@@ -1,16 +1,21 @@
 const fs = require('fs');
 const path = require('path');
 
-function GetDeepFileList(dir, callback, fileList = []) {
+function GetDeepFileList(dir, callback, baseDir, fileList = []) {
+    if (!baseDir) {
+      baseDir = dir
+    }
+
     try {
         const files = fs.readdirSync(dir, { withFileTypes: true });
         for (const file of files) {
             const filePath = path.join(dir, file.name);
+            const relativePath = path.relative(baseDir, filePath);
             if (file.isDirectory()) {
-                GetDeepFileList(filePath, callback, fileList);
+                GetDeepFileList(filePath, callback, baseDir, fileList);
             } else {
                 if (callback(file)) {
-                  fileList.push(filePath);
+                  fileList.push(relativePath);
                 }
                   
             }
