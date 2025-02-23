c0c3269a4577ab2f57a94443a71acf8084e7a2fb
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2025-02-23 18:14:31 +0800

    西元2025年02月23日 (週日) 18時14分31秒 CST

--- app/index-clone/IsIndexFile.js
+++ app/index-clone/IsIndexFile.js
@@ -3,6 +3,7 @@
 const isIndexFile = function (file) {
   // console.log(file)
   return file.endsWith('.note.xlsx') || 
+    file.endsWith('.list.xlsx') || 
     file.endsWith('.note.docx') ||
     file.startsWith('cluster-windows')
 }
